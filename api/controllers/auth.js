63d065c1cd118f86ed7c67aced64595d894d30d2
Adithya Saish <adiwithmj@gmail.com>
2022-07-19 20:10:00 +0530

    commiting UI

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -19,6 +19,7 @@ export const register = async (req, res, next) => {
 };
 
 export const login = async (req, res, next) => {
+  console.log(req.body);
   try {
     const user = await User.findOne({ username: req.body.username });
     if (user) {
